c5ce7539c44abdef593324870f6911def4353e6a
Mark R <mwritchie2@gmail.com>
2016-11-09 17:36:16 -0600

    blogging engaged

---
+++ blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    if @article.save
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @article.remove
+  end
+
+  protected
+
+end
